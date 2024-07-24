9933b818477372d1d2273c3d3615d6990587b018
Diego Alves <alvessd@live.com>
2024-07-23 22:19:48 -0300

    use iterator peekable

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,6 @@
+use std::iter::Peekable;
+use std::str::Chars;
+
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
     BeginParethesis,
@@ -6,7 +9,7 @@ pub enum TokenType {
     Asterisk,
     Integer(i64),
     Print,
-    Eof
+    Eof,
 }
 
 #[derive(Debug, PartialEq)]
@@ -21,53 +24,48 @@ impl Token {
     }
 
     pub fn eof(line: usize) -> Token {
-        Token { kind: TokenType::Eof, line }
+        Token {
+            kind: TokenType::Eof,
+            line,
+        }
     }
 }
 
-// TODO: use iterator peakable (https://doc.rust-lang.org/std/iter/struct.Peekable.html)
-pub struct Buffer {
-    opening: String,
-    opening_chars: Vec<char>,
-    pos: usize, // equals to size_t of C
-    pub line: usize
+pub struct Buffer<'a> {
+    chars: Peekable<Chars<'a>>,
+    pub line: usize,
 }
 
-impl Buffer {
-    pub fn create_com_string(s: &str) -> Buffer {
+impl<'a> Buffer<'a> {
+    pub fn create_com_string(s: &'a str) -> Buffer<'a> {
         Buffer {
-            opening: s.to_string(),
-            opening_chars: s.to_string().chars().collect(),
-            pos: 0,
-            line: 1
+            chars: s.chars().peekable(),
+            line: 1,
         }
     }
 
-    pub fn is_end(&self) -> bool {
-        self.pos >= self.opening_chars.len()
+    pub fn is_end(&mut self) -> bool {
+        self.chars.peek().is_none()
     }
 
     pub fn next_char(&mut self) -> Option<char> {
-        if self.is_end() {
-            None
-        } else {
-            let result = self.opening_chars[self.pos];
-            self.pos += 1;
-            Some(result)
-        }
+        self.chars.next()
     }
 
-    pub fn back_pos(&mut self) {
-        self.pos -= 1;
+    pub fn peek_char(&mut self) -> Option<&char> {
+        self.chars.peek()
     }
 
-    // check empty spaces in string, update position
     pub fn empty_space(&mut self) {
-        while !self.is_end() && self.opening_chars[self.pos].is_whitespace() {
-            if self.opening_chars[self.pos] == '\n' {
-                self.line += 1;
+        while let Some(&c) = self.peek_char() {
+            if c.is_whitespace() {
+                if c == '\n' {
+                    self.line += 1;
+                }
+                self.next_char();
+            } else {
+                break;
             }
-            self.pos += 1;
         }
     }
 }
@@ -88,43 +86,42 @@ pub fn next_token(buffer: &mut Buffer) -> Token {
         Some(c) if c.is_digit(10) => number_token(buffer, c),
         Some(c) if c.is_alphabetic() => word_token(buffer, c),
         None => Token::eof(buffer.line),
-        Some(c) => panic!("unexpected character: {} at line {}", c, buffer.line)
+        Some(c) => panic!("unexpected character: {} at line {}", c, buffer.line),
     }
 }
 
 pub fn number_token(buffer: &mut Buffer, c: char) -> Token {
     // accumulate the digits in string, then convert to integer
-    let mut digits: Vec<char> = vec!(c);
-    let mut next = buffer.next_char();
+    let mut digits: Vec<char> = vec![c];
 
-    while next.is_some() && next.expect("q?").is_digit(10) {
-        digits.push(next.expect("q?"));
-        next = buffer.next_char();
-    }
-
-    // return the last character read to the buffer
-    if next.is_some() {
-        buffer.back_pos();
+    while let Some(&next) = buffer.peek_char() {
+        if next.is_digit(10) {
+            digits.push(next);
+            buffer.next_char();
+        } else {
+            break;
+        }
     }
 
     let s: String = digits.iter().collect();
-    let value = i64::from_str_radix(&s, 10).
-        expect("error while converting from string to number");
+    let value = i64::from_str_radix(&s, 10).expect("error while converting from string to number");
 
-    Token { kind: TokenType::Integer(value), line: buffer.line }
+    Token {
+        kind: TokenType::Integer(value),
+        line: buffer.line,
+    }
 }
 
 pub fn word_token(buffer: &mut Buffer, c: char) -> Token {
-    let mut letters: Vec<char> = vec!(c);
-    let mut next = buffer.next_char();
+    let mut letters: Vec<char> = vec![c];
 
-    while next.is_some() && next.expect("q?").is_alphabetic() {
-        letters.push(next.expect("q?"));
-        next = buffer.next_char();
-    }
-
-    if next.is_some() {
-        buffer.back_pos();
+    while let Some(&next) = buffer.peek_char() {
+        if next.is_alphabetic() {
+            letters.push(next);
+            buffer.next_char();
+        } else {
+            break;
+        }
     }
 
     let s: String = letters.iter().collect();
@@ -132,10 +129,14 @@ pub fn word_token(buffer: &mut Buffer, c: char) -> Token {
         panic!("unrecognized keyword: {} at line {}", s, buffer.line);
     }
 
-    Token { kind: TokenType::Print, line: buffer.line }
+    Token {
+        kind: TokenType::Print,
+        line: buffer.line,
+    }
 }
 
 #[test]
+#[rustfmt::skip]
 fn test1_lexer() {
     let test1 = "( + * print  \n\n43257)   ";
     let mut buffer1 = Buffer::create_com_string(test1);
